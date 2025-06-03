032be8dd41177a855b1107ff07f3a8e5bb0c4d74
IsaakuTK <isaaccallecardozo@gmail.com>
2025-06-03 15:57:52 -0500

    modify

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-const repoName = "mi-portafolio"; // Cambia esto por el nombre de tu repositorio
-
 const nextConfig: NextConfig = {
-  output: "export", // Necesario para GitHub Pages (exportación estática)
-  basePath: isProd ? `/${repoName}` : "", // Ajusta las rutas para GitHub Pages
-  assetPrefix: isProd ? `/${repoName}/` : "", // Para que carguen CSS/imágenes
-  devIndicators: false, // Opcional: desactiva indicadores de desarrollo
-  // Otras configuraciones personalizadas...
+  /* config options here */
+  devIndicators: false
 };
 
 export default nextConfig;
